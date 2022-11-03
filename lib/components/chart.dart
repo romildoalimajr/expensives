f63c654e1df50581f35d5e9b15b7d192ecd1f959
romildoalimajr <odlimor.sevla@gmail.com>
2022-11-02 23:29:32 -0300

    new file:   assets/image/waiting.png 	new file:   lib/components/chart.dart 	modified:   lib/components/chart.dart 	modified:   lib/components/transaction_form.dart 	modified:   lib/components/transaction_list.dart 	modified:   lib/main.dart 	modified:   pubspec.yaml 	lib/components/chart_bar.dart

---
+++ lib/components/chart.dart
@@ -0,0 +1,26 @@
+import 'package:expensives/models/transaction.dart';
+import 'package:flutter/material.dart';
+import '../models/transaction.dart';
+
+class Chart extends StatelessWidget {
+  final List<Transaction> recentTransactions;
+
+  Chart(this.recentTransactions);
+
+  List<Map<String, Object>> get groupedTransactions {
+    return List.generate(7, (index) {
+      return {'day': 'T', 'value': 9.99};
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      elevation: 6,
+      margin: EdgeInsets.all(20),
+      child: Row(
+        children: <Widget>[],
+      ),
+    );
+  }
+}
